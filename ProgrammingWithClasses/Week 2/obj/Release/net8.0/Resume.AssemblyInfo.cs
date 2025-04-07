305dc66cdc1f03e045e37b6846c752b700632b47
MatheusMalaquias <mateusotavio614@gmail.com>
2025-04-07 15:09:54 -0300

    Updated

--- ProgrammingWithClasses/Week 2/obj/Release/net8.0/Resume.AssemblyInfo.cs
+++ ProgrammingWithClasses/Week 2/obj/Release/net8.0/Resume.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Resume")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47320445ca94d6e1c08b3196372c6cfa7150537a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31da5eb45e39206195b3146f249ad613e42e569a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Resume")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Resume")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
